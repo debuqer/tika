f87ad757d9e8a345b10081c20607eb6d3f821547
debuqer <bagher2g@gmail.com>
2022-06-13 20:39:20 +0430

    framework structure

--- public/index.php
+++ public/index.php
@@ -1,6 +1,5 @@
 <?php
 
-require_once __DIR__.'/../vendors/core/vendor/autoload.php';
-$vendorDirectory = 'core';
+require_once __DIR__.'/../vendor/autoload.php';
 
-require_once __DIR__.'/../vendors/'.$vendorDirectory.'/vendor/autoload.php';
+echo 'hello world';
