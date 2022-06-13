3ef1d6cf7ad369d6195c1ee26e03f25d53370024
debuqer <bagher2g@gmail.com>
2022-06-13 20:33:01 +0430

    basic autoload for project

--- public/index.php
+++ public/index.php
@@ -1,8 +1,6 @@
 <?php
 
-require_once __DIR__.'/../vendors/helpers.php';
-
-$vendorDirectory = findVendorDiretory();
-echo $vendorDirectory;
+require_once __DIR__.'/../vendors/core/vendor/autoload.php';
+$vendorDirectory = 'core';
 
 require_once __DIR__.'/../vendors/'.$vendorDirectory.'/vendor/autoload.php';
