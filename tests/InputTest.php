983528e29a322f9e02ce56e18a58b0edc68ba948
debuqer <bagher2g@gmail.com>
2022-06-29 20:33:04 +0430

    input test

--- tests/InputTest.php
+++ tests/InputTest.php
@@ -1,22 +1,22 @@
 <?php
 
 
-class ItemTest extends \PHPUnit\Framework\TestCase
+class InputTest extends \PHPUnit\Framework\TestCase
 {
     /**
      * @test
      */
-    public function test_fresh_item_schema_is_empty()
+    public function test_fresh_input_schema_has_attributes_index()
     {
         $item = new \Debuqer\Tika\Items\InputTypes\Input([]);
 
-        $this->assertEmpty($item->getSchema());
+        $this->assertArrayHasKey('attributes', $item->getSchema());
     }
 
     /**
      * @test
      */
-    public function test_fresh_item_name_is_empty()
+    public function test_fresh_input_name_is_empty()
     {
         $item = new \Debuqer\Tika\Items\InputTypes\Input([]);
 
@@ -26,7 +26,7 @@ class ItemTest extends \PHPUnit\Framework\TestCase
     /**
      * @test
      */
-    public function test_fresh_item_label_is_empty()
+    public function test_fresh_input_label_is_empty()
     {
         $item = new \Debuqer\Tika\Items\InputTypes\Input([]);
 
@@ -36,7 +36,7 @@ class ItemTest extends \PHPUnit\Framework\TestCase
     /**
      * @test
      */
-    public function test_item_name_set_properly()
+    public function test_input_name_set_properly()
     {
         $item = new \Debuqer\Tika\Items\InputTypes\Input([]);
         $item->setName('myInputName');
@@ -47,7 +47,7 @@ class ItemTest extends \PHPUnit\Framework\TestCase
     /**
      * @test
      */
-    public function test_item_label_set_properly()
+    public function test_input_label_set_properly()
     {
         $item = new \Debuqer\Tika\Items\InputTypes\Input([]);
         $item->setLabel('myInputLabel');
@@ -58,7 +58,7 @@ class ItemTest extends \PHPUnit\Framework\TestCase
     /**
      * @test
      */
-    public function test_item_attributes_works_properly()
+    public function test_input_attributes_works_properly()
     {
         $attributes = [
             ['provider' => \Debuqer\Tika\Items\InputTypes\Attributes\AcceptAttribute::class, 'params' => 'file_extension']
@@ -69,6 +69,6 @@ class ItemTest extends \PHPUnit\Framework\TestCase
         $schema = $item->getSchema();
 
         $this->assertNotEmpty($schema);
-        $this->assertArrayHasKey('accept', $schema);
+        $this->assertArrayHasKey('attributes', $schema);
     }
 }
