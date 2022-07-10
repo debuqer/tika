44a771ca4292d988077bfc4ff0880aac65a7da17
debuqer <bagher2g@gmail.com>
2022-07-10 14:09:02 +0430

    options setter and getters

--- src/Items/InputTypes/SelectInput.php
+++ src/Items/InputTypes/SelectInput.php
@@ -4,11 +4,36 @@
 namespace Debuqer\Tika\Items\InputTypes;
 
 
+use Debuqer\Tika\Items\Attributes\OptionsAttribute;
 use Debuqer\Tika\Items\Attributes\TypeAttribute;
 
 class SelectInput extends Input
 {
     protected $attributes = [
-        ['provider' => TypeAttribute::class, 'params' => 'text']
+        ['provider' => OptionsAttribute::class, 'params' => []]
     ];
+
+    protected $options = [];
+
+    public function __construct($options = [], $attributes)
+    {
+        $this->options = $options;
+        parent::__construct(array_merge($this->attributes, $attributes));
+    }
+
+    /**
+     * @param $options
+     */
+    public function setOptions($options)
+    {
+        $this->options = $options;
+    }
+
+    /**
+     * @return array
+     */
+    public function getOptions()
+    {
+        return $this->options;
+    }
 }
