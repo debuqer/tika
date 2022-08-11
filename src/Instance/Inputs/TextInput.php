da37d06839020395d7c9a12981ae154c8571bfe3
debuqer <bagher2g@gmail.com>
2022-08-11 15:07:53 +0430

    add type constraint to text input

--- src/Instance/Inputs/TextInput.php
+++ src/Instance/Inputs/TextInput.php
@@ -3,5 +3,10 @@ namespace Debuqer\Tika\Instance\Inputs;
 
 class TextInput extends BaseInput
 {
-
+    public function getItemValidations()
+    {
+        return [
+            'type' => ['type' => 'string']
+        ];
+    }
 }
